4d0b9501d12434a36dbe826603311d6aa3365024
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2020-07-28 16:21:07 +0500

    fix null mail

--- send.php
+++ send.php
@@ -1,4 +1,10 @@
 <?php
+
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8\r\n";
+$headers .= "From: noreply@accounting-website.ru\r\n";
+
+
 $tel = $_POST['tel'];
 $tel = htmlspecialchars($tel);
 $tel = urldecode($tel);
@@ -8,11 +14,18 @@ $name = $_POST['name'];
 $name = htmlspecialchars($name);
 $name = urldecode($name);
 $name = trim($name);
-//echo $tel;
-//echo "<br>";
-//echo $email;
-if (mail("timur53shakirov@mail.ru", "Заявка с сайта", $name." ждёт звонка " ."на номер: ".$tel,"From: accounting-website@timshaq.github.io \r\n"))
- {     echo "ok";
-} else {
-    echo "fail";
-}?>
+
+$to          = "timur53shakirov@mail.ru";
+$subject     = "Заявка с сайта";
+$text        = $name;
+$text       .= " ждёт звонка на номер ";
+$text       .= $tel;
+
+
+$sendToMail  = mail($to,$subject,$text,$headers);
+
+if ($sendToMail) {
+  echo "ok";
+}
+
+?>
--- src/js/form.js
+++ src/js/form.js
@@ -1,33 +1,40 @@
 $(function() {
   $("#input--tel").mask("+7 (999) 999-99-99");
 
-  var inpTel = $("#input--tel");
-  var inpName = $("#input--name");
-  var btn = $('#send-form');
+  var formNodeList = document.querySelectorAll('form');
+  var formList = Array.prototype.slice.call(formNodeList);
 
-  btn.on('click', function(ev) {
-    ev.preventDefault;
-    if (inpTel[0].value.length === 18) {
-      if (inpName[0].validity.patternMismatch) {
-        alert('Поле "Ваше Имя" должно содержать в себе только буквы (не меньше двух)');
-        inpName.addClass('error-input');
-        setTimeout(function(){inpName.removeClass('error-input')}, 2000)
-      } else {
-        console.log('send php')
-        $.post( "send.php", {'tel': inpTel[0].value, 'name': inpName[0].value},
-					function( data ) {
-				  		if (data === 'ok') {
-                alert('Заявка отправлена, скоро мы вам перезвоним');
-              } else {
-                alert('Произошла ошибка отправки, попробуйте снова')
-              }
-				});
-      }
+  function getSendResult (res) {
+    console.log(res)
+    if (res != 'ok') {
+      alert('Ошибка отправки, попробуйте еще раз!')
     } else {
-      alert('Поле "Ваш телефон" должно быть заполнено');
-      inpTel.addClass('error-input');
-      setTimeout(function(){inpTel.removeClass('error-input')}, 2000)
-
+      alert('Ваша заявка отправлена!')
     }
+  }
+
+  formList.map(form => {
+    form.addEventListener('submit', function(ev) {
+      console.log('SUBMIT')
+      ev.preventDefault()
+      var sendBody = $(this).serialize()
+
+      console.log('sendBody')
+      console.log( sendBody )
+
+        fetch("send.php", { 
+            method: "POST",
+            body: sendBody,   
+            headers:{"content-type": "application/x-www-form-urlencoded"} 
+          })
+        .then(res => {return res.text()})
+        .then(text => {getSendResult(text)})
+
+        
+
+    })
+    
+
   })
+
 })
